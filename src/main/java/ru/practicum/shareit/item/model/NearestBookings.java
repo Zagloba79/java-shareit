101836c7de00d7f028a33663197b55b26755f8c9
Zagloba <an9262778931@yandex.ru>
2023-07-09 23:00:17 +0300

    доделал все комменты

--- src/main/java/ru/practicum/shareit/item/model/NearestBookings.java
+++ src/main/java/ru/practicum/shareit/item/model/NearestBookings.java
@@ -4,14 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class NearestBookings {
+    @NotNull
     private LocalDateTime previousBookingStart;
+    @NotNull
     private LocalDateTime previousBookingEnd;
+    @NotNull
     private LocalDateTime nextBookingStart;
+    @NotNull
     private LocalDateTime nextBookingEnd;
 }
