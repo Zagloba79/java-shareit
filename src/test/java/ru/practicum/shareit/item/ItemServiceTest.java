af64ba29c88e4a51f6f4f12cbbdd00924c76f1f2
Zagloba <an9262778931@yandex.ru>
2023-07-28 20:51:27 +0300

    пишу тесты

---
+++ src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
@@ -0,0 +1,82 @@
+package ru.practicum.shareit.item;
+
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.exception.ObjectNotFoundException;
+import ru.practicum.shareit.handleAndValidate.EntityHandler;
+import ru.practicum.shareit.item.dto.*;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.UserRepository;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ItemServiceTest {
+    private final EasyRandom generator = new EasyRandom();
+    @InjectMocks
+    private ItemServiceImpl service;
+    @Mock
+    private ItemRepository repository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private EntityHandler handler;
+    User user = new User("user", "user@user.ru");
+    UserDto userDto = new UserDto("user", "user@user.ru");
+    ItemDto itemDto = new ItemDto("itemDto", "itemDtoDescription",
+            true, null);
+    Item item = new Item("itemDto", "itemDtoDescription", true, user, null);
+
+    @Test
+    public void createAndUpdateItemTest() {
+        user.setId(1L);
+        doNothing().when(handler).itemValidate(any());
+        when(handler.getUserFromOpt(user.getId())).thenReturn(user);
+        when(repository.save(Mockito.any(Item.class)))
+                .thenReturn(item);
+        ItemDto itemFromDb = service.create(itemDto, user.getId());
+        assertEquals("itemDto", itemFromDb.getName());
+        assertEquals("itemDtoDescription", itemFromDb.getDescription());
+        ItemDto newItemDto = new ItemDto("new", null,
+                true, null);
+        when(handler.getItemFromOpt(itemFromDb.getId())).thenReturn(ItemMapper.createItem(itemFromDb, user));
+        ItemDto updatedItemDto = service.updateItem(newItemDto, itemFromDb.getId(), user.getId());
+        assertEquals("new", updatedItemDto.getName());
+        assertEquals("itemDtoDescription", updatedItemDto.getDescription());
+        newItemDto = new ItemDto(null, "notNull",
+                true, null);
+        updatedItemDto = service.updateItem(newItemDto, itemFromDb.getId(), user.getId());
+        assertEquals("new", updatedItemDto.getName());
+        assertEquals("notNull", updatedItemDto.getDescription());
+    }
+
+//    @Test
+//    void exceptionWhenGetItemWithWrongId() {
+//        user.setId(1L);
+//        long itemId = 234L;
+//        when(handler.getUserFromOpt(user.getId())).thenReturn(user);
+//        when(repository.findById(itemId)).thenReturn(Optional.empty());
+//        when(handler.getItemFromOpt(itemId)).thenThrow(ObjectNotFoundException.class);
+////        ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class,
+////                () -> handler.getItemFromOpt(itemId));
+//        ObjectNotFoundException exception = assertThrows(ObjectNotFoundException.class,
+//                () -> service.getItemById(itemId, user.getId()));
+//        assertEquals("Предмета с " + itemId + " не существует.", exception.getMessage());
+//
+//    }
+}
