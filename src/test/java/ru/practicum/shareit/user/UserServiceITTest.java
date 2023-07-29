4606dcf646432178e610af335060e4e0d43e4ab0
Zagloba <an9262778931@yandex.ru>
2023-07-30 02:19:43 +0300

    пишу тесты

--- src/test/java/ru/practicum/shareit/user/UserServiceITTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceITTest.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
