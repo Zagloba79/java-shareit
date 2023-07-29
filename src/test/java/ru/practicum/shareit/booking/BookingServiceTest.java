cc8214949f529f4d6df2660eb6d5d5bf5c45fe88
Zagloba <an9262778931@yandex.ru>
2023-07-29 09:27:18 +0300

    пишу тесты

--- src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
@@ -1,25 +1,15 @@
 package ru.practicum.shareit.booking;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.BookingMapper;
+
 import ru.practicum.shareit.booking.dto.NewBookingDto;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.exception.OperationIsNotSupported;
 import ru.practicum.shareit.handleAndValidate.EntityHandler;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDateTime;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static ru.practicum.shareit.booking.model.BookingStatus.WAITING;
 
 @ExtendWith(MockitoExtension.class)
 public class BookingServiceTest {
@@ -42,26 +32,6 @@ public class BookingServiceTest {
 //        when(handler.getItemFromOpt(item.getId())).thenReturn(item);
 //        doNothing().when(handler).itemIsAvailable(item);
 //        when(repository.save(booking)).thenReturn(booking);
-//    }
-//
-//
-//    public BookingDto create(NewBookingDto newBookingDto, Long bookerId) {
-//        LocalDateTime presentTime = LocalDateTime.now();
-//        handler.bookingValidate(newBookingDto, presentTime);
-//        Item item = handler.getItemFromOpt(newBookingDto.getItemId());
-//        handler.itemIsAvailable(item);
-//        User booker = handler.getUserFromOpt(bookerId);
-//        if (item.getOwner().getId().equals(bookerId)) {
-//            throw new OperationIsNotSupported("Букер не может быть владельцем");
-//        }
-//        Booking booking = BookingMapper.createNewBooking(
-//                newBookingDto.getStart(),
-//                newBookingDto.getEnd(),
-//                item,
-//                booker);
-//        booking.setStatus(WAITING);
-//        bookingRepository.save(booking);
-//        return BookingMapper.createBookingDto(booking);
 //    }
 }
 
