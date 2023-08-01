52813afddcca9bcd4fceae47e208161ccee192ce
Zagloba <an9262778931@yandex.ru>
2023-08-01 22:19:05 +0300

    исправляю замечания

--- src/test/java/ru/practicum/shareit/request/ItemRequestDtoTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestDtoTest.java
@@ -1,28 +1,43 @@
 package ru.practicum.shareit.request;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.booking.dto.NewBookingDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.user.model.User;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @JsonTest
 public class ItemRequestDtoTest {
     private final JacksonTester<ItemRequestDto> json;
+    private final Validator validator;
 
     public ItemRequestDtoTest(@Autowired JacksonTester<ItemRequestDto> json) {
         this.json = json;
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 
     private final User requester = new User("ann", "ann@user.ru");
+    private final ItemRequestDto itemRequestDto = new ItemRequestDto("desc",
+            LocalDateTime.of(2023, 1, 2, 3, 4, 5));
+
 
     @Test
+    @DirtiesContext
     public void testJsonItemRequestDto() throws Exception {
         requester.setId(1L);
         ItemRequestDto itemRequestDto = new ItemRequestDto(
@@ -36,5 +51,36 @@ public class ItemRequestDtoTest {
         assertThat(result).extractingJsonPathNumberValue("$.requester.id").isEqualTo(1);
         assertThat(result).extractingJsonPathStringValue("$.created").isEqualTo("2023-01-02T03:04:05");
         assertThat(result).extractingJsonPathStringValue("$.requester.name").isEqualTo("ann");
+    }
+
+    @Test
+    @DirtiesContext
+    public void whenItemRequestDtoIsValid() {
+        itemRequestDto.setId(1L);
+        itemRequestDto.setRequester(requester);
+        Set<ConstraintViolation<ItemRequestDto>> violations = validator.validate(itemRequestDto);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    @DirtiesContext
+    public void whenDescriptionIsNull() {
+        itemRequestDto.setId(1L);
+        itemRequestDto.setRequester(requester);
+        itemRequestDto.setDescription(null);
+        Set<ConstraintViolation<ItemRequestDto>> violations = validator.validate(itemRequestDto);
+        assertThat(violations).isNotEmpty();
+        AssertionsForClassTypes.assertThat(violations.toString()).contains("interpolatedMessage='must not be null'");
+    }
+
+    @Test
+    @DirtiesContext
+    public void whenRequesterIsNull() {
+        itemRequestDto.setId(1L);
+        itemRequestDto.setRequester(null);
+        itemRequestDto.setDescription("null");
+        Set<ConstraintViolation<ItemRequestDto>> violations = validator.validate(itemRequestDto);
+        assertThat(violations).isNotEmpty();
+        AssertionsForClassTypes.assertThat(violations.toString()).contains("interpolatedMessage='must not be null'");
     }
 }
--- src/test/java/ru/practicum/shareit/request/RequestWithItemsDtoTest.java
+++ src/test/java/ru/practicum/shareit/request/RequestWithItemsDtoTest.java
@@ -1,5 +1,42 @@
 package ru.practicum.shareit.request;
 
-public class RequestWithItemsDtoTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.item.dto.ItemForAnswerDto;
+import ru.practicum.shareit.request.dto.RequestWithItemsDto;
+import ru.practicum.shareit.user.model.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@JsonTest
+public class RequestWithItemsDtoTest {
+    private final JacksonTester<RequestWithItemsDto> json;
+
+    public RequestWithItemsDtoTest(@Autowired JacksonTester<RequestWithItemsDto> json) {
+        this.json = json;
+    }
+    private final User owner = new User("alex", "alex@user.ru");
+    private final User requester = new User("ann", "ann@user.ru");
+
+    @Test
+    public void testJsonRequestWithItemsDto() throws Exception {
+        ItemForAnswerDto item = new ItemForAnswerDto("name", "desc", 5L, true);
+        item.setId(1L);
+        requester.setId(2L);
+        RequestWithItemsDto requestWithItemsDto = new RequestWithItemsDto(
+               "Description",
+                LocalDateTime.of(2023, 10, 11, 12, 13, 14),
+                List.of(item));
+        requestWithItemsDto.setId(5L);
+        JsonContent<RequestWithItemsDto> result = json.write(requestWithItemsDto);
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(5);
+        assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo("Description");
+        assertThat(result).extractingJsonPathStringValue("$.created").isEqualTo("2023-10-11T12:13:14");
+    }
 }
