c24927d7eef566025c7b536f28c4ed2a2588f400
Zagloba <an9262778931@yandex.ru>
2023-08-09 23:48:50 +0300

    ТЗ-16

--- gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerITTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerITTest.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.booking;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
@@ -13,19 +15,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static ru.practicum.shareit.booking.dto.BookingState.ALL;
 
+@ExtendWith(MockitoExtension.class)
 public class BookingControllerITTest {
     @Mock
     private BookingClient client;
     @InjectMocks
     private BookingController controller;
-    private BookItemRequestDto bookingDto = new BookItemRequestDto(1L,
+    private final BookItemRequestDto bookingDto = new BookItemRequestDto(1L,
             LocalDateTime.now().plusSeconds(1),
             LocalDateTime.now().plusSeconds(10));
     private final ResponseEntity<Object> objectResponseEntity = new ResponseEntity<>(HttpStatus.OK);
 
     @Test
     public void createBookingTest() {
-        Long userId = 1L;
+        long userId = 1L;
         when(client.bookItem(userId, bookingDto)).thenReturn(objectResponseEntity);
         ResponseEntity<Object> objectResponseEntity1 = controller.create(userId, bookingDto);
         assertEquals(objectResponseEntity, objectResponseEntity1);
@@ -43,7 +46,7 @@ public class BookingControllerITTest {
 
     @Test
     public void findBookingTest() {
-        Long userId = 1L;
+        long userId = 1L;
         Long bookingId = 2L;
         when(client.getBooking(userId, bookingId)).thenReturn(objectResponseEntity);
         ResponseEntity<Object> booking = controller.getBooking(userId, bookingId);
