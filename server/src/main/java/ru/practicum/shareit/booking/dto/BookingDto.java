4781b4cc3d8d09554e756c319070dd62f7c24e96
Zagloba <an9262778931@yandex.ru>
2023-08-10 00:35:20 +0300

    ТЗ-16

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -5,7 +5,6 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
@@ -15,7 +14,6 @@ import java.time.LocalDateTime;
 public class BookingDto {
     private Long id;
     private LocalDateTime start;
-    @NotNull
     private LocalDateTime end;
     private Item item;
     private User booker;
